dfb80dcf29153912bf54579161301a193cadeb86
Louie McConnell <louie_mc@calvisitor-10-105-133-25.calvisitor.1918.berkeley.edu>
2017-09-18 21:03:36 -0700

    added UI elements to opening view controller; no targets yet, stil lhave to add

--- Pokedex/OpeningViewController.swift
+++ Pokedex/OpeningViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OpeningViewController: UIViewController {
+    
+    var searchLabel: UILabel!
+    var categoryButton: UIButton!
+    var nameButton: UIButton!
+    var randomButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        addSearchLabel()
+        addCategoryButton()
+        addNameButton()
+        addRandomButton()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,65 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func addSearchLabel() {
+        searchLabel = UILabel()
+        searchLabel.frame = CGRect(
+            x: self.view.center.x - 60,
+            y: self.view.frame.height * 0.35,
+            width: 120,
+            height: 50
+        )
+        searchLabel.text = "search by: "
+        searchLabel.textAlignment = .center
+        self.view.addSubview(searchLabel)
+    }
+    
+    func addCategoryButton() {
+        categoryButton = UIButton()
+        categoryButton.frame = CGRect(
+            x: self.view.center.x - 80,
+            y: searchLabel.frame.maxY + 10,
+            width: 160,
+            height: 40
+        )
+        categoryButton.setTitle("category", for: .normal)
+        categoryButton.setTitleColor(UIColor.white, for: .normal)
+        categoryButton.layer.cornerRadius = 16
+        categoryButton.backgroundColor = UIColor.blue
+        categoryButton.contentHorizontalAlignment = .center
+        self.view.addSubview(categoryButton)
+    }
+    
+    func addNameButton() {
+        nameButton = UIButton()
+        nameButton.frame = CGRect(
+            x: self.view.center.x - 80,
+            y: categoryButton.frame.maxY + 10,
+            width: 160,
+            height: 40
+        )
+        nameButton.setTitle("name/number", for: .normal)
+        nameButton.setTitleColor(UIColor.white, for: .normal)
+        nameButton.layer.cornerRadius = 16
+        nameButton.backgroundColor = UIColor.blue
+        nameButton.contentHorizontalAlignment = .center
+        self.view.addSubview(nameButton)
+    }
+    
+    func addRandomButton() {
+        randomButton = UIButton()
+        randomButton.frame = CGRect(
+            x: self.view.center.x - 80,
+            y: nameButton.frame.maxY + 10,
+            width: 160,
+            height: 40
+        )
+        randomButton.setTitle("random", for: .normal)
+        randomButton.setTitleColor(UIColor.white, for: .normal)
+        randomButton.layer.cornerRadius = 16
+        randomButton.backgroundColor = UIColor.blue
+        randomButton.contentHorizontalAlignment = .center
+        self.view.addSubview(randomButton)
+    }
 }
 
