49a399122035f23f4c90e185a2bceb5ccc04e11d
Louie McConnell <louie_mc@calvisitor-10-105-134-158.calvisitor.1918.berkeley.edu>
2017-09-20 09:18:45 -0700

    committing so I can change

--- Pokedex/PokemonCollectionViewCell.swift
+++ Pokedex/PokemonCollectionViewCell.swift
@@ -33,37 +33,4 @@ class PokemonCollectionViewCell: UICollectionViewCell {
         pokemonImage.contentMode = .scaleAspectFill
         contentView.addSubview(pokemonImage)
     }
-    
-    func getPokemonImage() -> String {
-        let pokemonImageURL = URL(string: pokemonObject.imageUrl)
-        
-        // Creating a session object with the default configuration.
-        // You can read more about it here https://developer.apple.com/reference/foundation/urlsessionconfiguration
-        let session = URLSession(configuration: .default)
-        
-        // Define a download task. The download task will download the contents of the URL as a Data object and then you can do what you wish with that data.
-        let downloadPicTask = session.dataTask(with: pokemonImageURL) { (data, response, error) in
-            // The download has finished.
-            if let e = error {
-                print("Error downloading pokemon picture: \(e)")
-            } else {
-                // No errors found.
-                // It would be weird if we didn't have a response, so check for that too.
-                if let res = response as? HTTPURLResponse {
-                    print("Downloaded pokemon picture with response code \(res.statusCode)")
-                    if let imageData = data {
-                        // Finally convert that Data into an image and do what you wish with it.
-                        let image = UIImage(data: imageData)
-                        // Do something with your image.
-                    } else {
-                        print("Couldn't get image: Image is nil")
-                    }
-                } else {
-                    print("Couldn't get response code for some reason")
-                }
-            }
-        }
-        
-    }
-    
 }
